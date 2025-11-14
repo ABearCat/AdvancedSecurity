e50103c6b41443375c993e40d9d1940315b91636
Daoxu <ydaoxu@gmail.com>
2025-11-14 09:46:21 -0500

    testing venv + extra libraries

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import random
+import cowsay
 
 def generate_random_number():
     return random.randint(1, 100)
@@ -6,3 +7,5 @@ def generate_random_number():
 for _ in range(random.randint(1, 100)):
     print("Generating random numbers")
     print("Random number:", generate_random_number())
+cowsay.cow("Cow Test!")
+cowsay.cow(generate_random_number())
